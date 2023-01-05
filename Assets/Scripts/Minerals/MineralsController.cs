ea8b7cd4c1b3f6fe667a3001ea3582935332fbfe
zero <debuggerzero@gmail.com>
2023-01-05 15:41:25 +0800

    生成金块

---
+++ Assets/Scripts/Minerals/MineralsController.cs
@@ -0,0 +1,83 @@
+using Assets.Scripts.Untils;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MineralsController : MonoBehaviour
+{
+    
+    private readonly List<GameObject> positionList = new();
+
+    public int score = 650;
+
+    private void Awake()
+    {
+        positionList.Clear();
+        CreateMinerals();
+    }
+
+    private void CreateMinerals()
+    {
+        int sumScore = 0;
+        for (int i = 0; sumScore <= score || i < GlobalConstant.GOLD_MIN_COUNT ; i++)
+        {
+            int probability = Random.Range(1, 11);
+            int goldScore = probability <= 6 ? Random.Range(GlobalConstant.GOLD_MIN_SCORE, GlobalConstant.GOLD_MIN_SCORE << 1)
+                                            : Random.Range(GlobalConstant.GOLD_MIN_SCORE << 1, GlobalConstant.GOLD_MAX_SCORE + 1);
+            sumScore += goldScore;
+            Vector3 vector = CreatePosition();
+            GameObject gold = CreateGold(vector.x, vector.y, goldScore);
+            positionList.Add(gold);
+        }
+    }
+
+    private Vector3 CreatePosition()
+    {
+        float x = 0.0f, y = 0.0f;
+        bool flag = true;
+        while (flag)
+        {
+            flag = false;
+            x = Random.Range(GlobalConstant.GAME_MIN_X, GlobalConstant.GAME_MAX_X);
+            y = Random.Range(GlobalConstant.GAME_MIN_Y, GlobalConstant.GAME_MAX_Y);
+            foreach (var item in positionList)
+            {
+                float temp_x = Mathf.Abs(x - item.transform.position.x);
+                float temp_y = Mathf.Abs(y - item.transform.position.y);
+                if (Mathf.Pow(temp_x, 2) + Mathf.Pow(temp_y, 2) < Mathf.Pow(item.GetComponent<CircleCollider2D>().radius * 2, 2))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        Vector3 vector = new Vector3(x, y, 0);
+        return vector;
+    }
+
+    private GameObject CreateGold(float x, float y, int score)
+    {
+        GameObject gold = new GameObject("gold");
+
+        gold.transform.parent = transform;
+        gold.transform.position = new Vector3(x, y, 0);
+        float scale = (1.5f - 0.3f) / (GlobalConstant.GOLD_MAX_SCORE - GlobalConstant.GOLD_MIN_SCORE) * score;
+        gold.transform.localScale = new Vector3(scale, scale, scale);
+        gold.AddComponent<SpriteRenderer>();
+        gold.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("gold");
+        gold.GetComponent<SpriteRenderer>().sortingLayerName = "Player";
+        //创建圆形碰撞体
+        gold.AddComponent<CircleCollider2D>();
+        gold.GetComponent<CircleCollider2D>().tag = "minerals";
+        gold.GetComponent<CircleCollider2D>().isTrigger = true;
+        gold.GetComponent<CircleCollider2D>().radius = 0.5f;
+
+        gold.AddComponent<GoldModel>();
+        gold.GetComponent<GoldModel>().score = score;
+
+        return gold;
+    }
+
+}
---
+++ Assets/Scripts/Untils/GlobalConstant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Untils
+{
+    public class GlobalConstant
+    {
+
+        #region 游戏边界坐标
+
+        public const float GAME_MAX_X = 6.6f;
+
+        public const float GAME_MIN_X = -6.6f;
+
+        public const float GAME_MAX_Y = 2.24f;
+
+        public const float GAME_MIN_Y = -3.35f;
+
+        #endregion
+
+        #region 金块
+
+        public const int GOLD_MIN_COUNT = 10;
+
+        public const int GOLD_MAX_SCORE = 500;
+
+        public const int GOLD_MIN_SCORE = 50;
+
+        #endregion
+    }
+}
